22a6e42f080dd7cea69a485a8a382b3dd4f6f170
Harmen Stoppels <harmenstoppels@gmail.com>
2024-05-06 18:16:24 +0200

    condense

--- waterfall.sh
+++ waterfall.sh
@@ -41,6 +41,43 @@ jq \
 'select(.started_at and .finished_at) | '\
 '{name: (.name), cat: "PERF", ph: "B", pid: .pipeline.id, tid: .id, ts: (.started_at | sub("\\.[0-9]+Z$"; "Z") | fromdate * 10e5)},'\
 '{name: (.name), cat: "PERF", ph: "E", pid: .pipeline.id, tid: .id, ts: (.finished_at | sub("\\.[0-9]+Z$"; "Z") | fromdate * 10e5)}'\
-']) | flatten(1) | .[]' jobs-$project-*.json | jq -s > trace.json
+']) | flatten(1) | .[]' jobs-$project-*.json | jq -s > trace_.json
 
-echo $(realpath trace.json)
+python3 - <<EOF
+import json
+from typing import Dict, List, Tuple
+from collections import defaultdict
+
+data = json.load(open("trace_.json"))
+data.sort(key=lambda x: (x["ts"], x["ph"] == "E"))
+processes: Dict[str, List[List[dict]]] = defaultdict(list)
+new_thread_id: Dict[Tuple[str, str], int] = {}
+
+def event_id(event):
+    return (event["pid"], event["tid"])
+
+for event in data:
+    process = processes[event["pid"]]
+
+    if event["ph"] == "B":
+        event_key = event_id(event)
+        for i, events in enumerate(process):
+            if events[-1]["ph"] == "E" and events[-1]["ts"] < event["ts"]:
+                events.append(event)
+                event["tid"] = i
+                new_thread_id[event_key] = i
+                break
+        else:
+            process.append([event])
+            event["tid"] = len(process) - 1
+            new_thread_id[event_key] = len(process) - 1
+    elif event["ph"] == "E":
+        event["tid"] = new_thread_id[event_id(event)]
+        process[event["tid"]].append(event)
+
+new_processes = [event for process in processes.values() for events in process for event in events]
+
+json.dump(new_processes, open("trace.json", "w"))
+EOF
+
+echo "$(realpath trace.json)"
